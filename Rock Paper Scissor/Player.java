0141a75e5424c4a87dc0fcbc01f83ca813c8990c
Mohammed Maruf Islam <64244354+MdMaruf-22@users.noreply.github.com>
2023-03-10 02:38:08 +0600

    Create Player.java

---
+++ Rock Paper Scissor/Player.java
@@ -0,0 +1,17 @@
+public abstract class Player{
+    private String name;
+    private String choice;
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setChoice(String choice){
+        this.choice=choice;
+    }
+    public String getChoice(){
+        return choice;
+    }
+    public abstract void selectChoice();
+}
